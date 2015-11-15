f69cfcedaf804f2437198b94ce8f76a4f6d0bad9
Caio Santana Magalhães <caiosm1005@gmail.com>
2015-11-15 02:42:30 -0300

    Create new display classes

--- public/core.js
+++ public/core.js
@@ -38,6 +38,41 @@ getStory( languageId, x, y, function( err, result ) {
     console.log( result );
 } );
 
+// Util functions
+// -----------------------------------------------------------------------------
+function wrapMessage( msg, breakingPoint ) {
+    // Try to break message into two lines if it's large enough
+    if ( msg.length <= breakingPoint ) {
+        return msg;
+    }
+
+    var textSplit = false;
+
+    for( var i = breakingPoint; i < msg.length && i < breakingPoint+10; i++ ) {
+        if ( msg[ i ] == " " ) {
+            msg = msg.substr( 0, i ) + "\n" + msg.substr( i + 1 );
+            textSplit = true;
+            break;
+        }
+    }
+
+    if ( ! textSplit ) {
+        for( var i = breakingPoint - 1; i >= 0 && i >= breakingPoint-10; i-- ) {
+            if ( msg[ i ] == " " ) {
+                msg = msg.substr( 0, i ) + "\n" + msg.substr( i + 1 );
+                textSplit = true;
+                break;
+            }
+        }
+    }
+
+    if ( ! textSplit ) {
+        msg = msg.substr( 0, breakingPoint )+"\n"+msg.substr( breakingPoint );
+    }
+
+    return msg;
+}
+
 // Display classes
 // -----------------------------------------------------------------------------
 var DisplayBackground = function() {
@@ -52,58 +87,65 @@ var DisplayBackground = function() {
     return rect;
 };
 
-var DisplayStory = function( message ) {
+var DisplayStoryBlobs = function( numBlobs, speed, radius ) {
     var container = new createjs.Container();
 
-    var circleTimeIncrement = 0.01;
-    var circle1Time = Math.random() * 10;
-    var circle2Time = Math.random() * 10;
-    var circle3Time = Math.random() * 10;
-    var circle1MaxOffset = [ Math.random() * 17 + 10, Math.random() * 17 + 10 ];
-    var circle2MaxOffset = [ Math.random() * 17 + 10, Math.random() * 17 + 10 ];
-    var circle3MaxOffset = [ Math.random() * 17 + 10, Math.random() * 17 + 10 ];
+    var circleTimes1 = [];
+    var circleTimes2 = [];
+    var circleOffsets = [];
+    var circles = [];
 
-    var circle1 = new createjs.Shape();
-    var circle2 = new createjs.Shape();
-    var circle3 = new createjs.Shape();
+    container.centerDeltaX = 0.0;
+    container.centerDeltaY = 0.0;
 
-    circle1.graphics.beginFill( "DeepSkyBlue" );
-    circle2.graphics.beginFill( "DeepSkyBlue" );
-    circle3.graphics.beginFill( "DeepSkyBlue" );
+    for( var i = 0; i < numBlobs; i++ ) {
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill( "DeepSkyBlue" );
+        circle.graphics.drawCircle( 0, 0, radius );
 
-    circle1.graphics.drawCircle( 0, 0, 170 );
-    circle2.graphics.drawCircle( 0, 0, 170 );
-    circle3.graphics.drawCircle( 0, 0, 170 );
+        var offsetX = Math.random() * radius / 10 + 10;
+        var offsetY = Math.random() * radius / 10 + 10;
+
+        circleTimes1.push( Math.random() * 10 );
+        circleTimes2.push( Math.random() * 10 );
+        circleOffsets.push( [ offsetX, offsetY ] );
+        circles.push( circle );
+        container.addChild( circle );
+    }
+
+    container.addEventListener( "tick", function() {
+        container.centerDeltaX = 1.0;
+        container.centerDeltaY = 1.0;
+
+        for( var i = 0; i < circles.length; i++ ) {
+            var deltaX = Math.cos( circleTimes1[i] );
+            var deltaY = Math.sin( circleTimes1[i] );
+
+            circles[ i ].x = deltaX * circleOffsets[i][0];
+            circles[ i ].y = deltaY * circleOffsets[i][1];
+            circles[ i ].scaleX = 1.0 + Math.cos( circleTimes2[ i ] ) * 0.07;
+            circles[ i ].scaleY = 1.0 + Math.sin( circleTimes2[ i ] ) * 0.07;
+
+            circleTimes1[ i ] += speed;
+            circleTimes2[ i ] += speed;
+
+            container.centerDeltaX *= deltaX;
+            container.centerDeltaY *= deltaY;
+        }
+    } );
+
+    return container;
+}
+
+var DisplayStory = function( message ) {
+    var container = new createjs.Container();
+    var displayBlobs = new DisplayStoryBlobs( 3, 0.0065, 170 );
 
     var baseline = "middle";
     var size = "140px";
 
-    // Try to break message into two lines if it's large enough
     if ( message.length > 15 ) {
-        var textSplit = false;
-
-        for( var i = 15; i < message.length && i < 30; i++ ) {
-            if ( message[ i ] == " " ) {
-                message = message.substr( 0, i ) + "\n" + message.substr( i+1 );
-                textSplit = true;
-                break;
-            }
-        }
-
-        if ( ! textSplit ) {
-            for( var i = 14; i >= 5; i-- ) {
-                if ( message[ i ] == " " ) {
-                    message = message.substr( 0, i )+"\n"+message.substr( i+1 );
-                    textSplit = true;
-                    break;
-                }
-            }
-        }
-
-        if ( ! textSplit ) {
-            message = message.substr( 0, 15 ) + "\n" + message.substr( 16 );
-        }
-
+        message = wrapMessage( message, 15 );
         baseline = "bottom";
     }
 
@@ -125,40 +167,66 @@ var DisplayStory = function( message ) {
     text.snapToPixel = true;
 
     container.addEventListener( "tick", function() {
-        var cosCircle1Time = Math.cos( circle1Time );
-        var cosCircle2Time = Math.cos( circle2Time );
-        var cosCircle3Time = Math.cos( circle3Time );
-        var sinCircle1Time = Math.sin( circle1Time );
-        var sinCircle2Time = Math.sin( circle2Time );
-        var sinCircle3Time = Math.sin( circle3Time );
-
-        circle1.x = cosCircle1Time * circle1MaxOffset[ 0 ];
-        circle1.y = sinCircle1Time * circle1MaxOffset[ 1 ];
-        circle1.scaleX = 1.0 + cosCircle3Time * 0.07;
-        circle1.scaleY = 1.0 + sinCircle3Time * 0.07;
-
-        circle2.x = cosCircle2Time * circle2MaxOffset[ 0 ];
-        circle2.y = sinCircle2Time * circle2MaxOffset[ 1 ];
-        circle2.scaleX = 1.0 + cosCircle1Time * 0.07;
-        circle2.scaleY = 1.0 + sinCircle1Time * 0.07;
-
-        circle3.x = cosCircle3Time * circle3MaxOffset[ 0 ];
-        circle3.y = sinCircle3Time * circle3MaxOffset[ 1 ];
-        circle3.scaleX = 1.0 + cosCircle2Time * 0.07;
-        circle3.scaleY = 1.0 + sinCircle2Time * 0.07;
-
-        text.x = cosCircle1Time * cosCircle2Time * cosCircle3Time * 6.2 - 5;
-        text.y = sinCircle1Time * sinCircle2Time * sinCircle3Time * 6.2;
-
-        circle1Time += circleTimeIncrement;
-        circle2Time += circleTimeIncrement;
-        circle3Time += circleTimeIncrement;
+        text.x = displayBlobs.centerDeltaX * 6.2 - 4;
+        text.y = displayBlobs.centerDeltaY * 6.2;
     } );
 
-    container.addChild( circle1 );
-    container.addChild( circle2 );
-    container.addChild( circle3 );
+    container.addChild( displayBlobs );
     container.addChild( text );
+
+    return container;
+};
+
+var DisplayNeighbour = function( message ) {
+    var container = new createjs.Container();
+    var displayBlobs = new DisplayStoryBlobs( 8, 0.015, 25 );
+
+    var baseline = "middle";
+
+    if ( message.length > 15 ) {
+        message = wrapMessage( message, 15 );
+        baseline = "bottom";
+    }
+
+    var text = new createjs.Text( message, "bold 24px 'Open Sans'", "#FFFFFF" );
+    text.textBaseline = baseline;
+    text.textAlign = "center";
+    text.maxWidth = 170;
+    text.scaleX = 0.5;
+    text.scaleY = 0.5;
+    text.cache( -90, -48, 180, 75 );
+    text.snapToPixel = true;
+
+    container.addEventListener( "tick", function() {
+        text.x = displayBlobs.centerDeltaX * 6.2;
+        text.y = displayBlobs.centerDeltaY * 6.2;
+    } );
+
+    var hitArea = new createjs.Shape();
+    hitArea.graphics.beginFill( "#FFF" );
+    hitArea.graphics.drawRect( -38, -38, 76, 76 );
+
+    container.addChild( displayBlobs );
+    container.addChild( text );
+
+    container.hitArea = hitArea;
+    container.cursor = "pointer";
+
+    container.on( "mouseover", function( event ) {
+        createjs.Tween.get( displayBlobs )
+            .to( { scaleX: 1.1, scaleY: 1.1 }, 350,
+                createjs.Ease.getPowOut( 2.8 ) );
+        createjs.Tween.get( text )
+            .to( { scaleX: 0.7, scaleY: 0.7 }, 280,
+                createjs.Ease.getBackOut( 4.0 ) );
+    } );
+
+    container.on( "mouseout", function( event ) {
+        createjs.Tween.get( displayBlobs )
+            .to( { scaleX: 1.0, scaleY: 1.0 }, 300, createjs.Ease.cubicOut );
+        createjs.Tween.get( text )
+            .to( { scaleX: 0.5, scaleY: 0.5 }, 300, createjs.Ease.cubicOut );
+    } );
 
     return container;
 };
@@ -168,29 +236,56 @@ var DisplayNeighbourPlus = function() {
 
     var circle = new createjs.Shape();
     circle.graphics.beginFill( "DeepSkyBlue" );
-    circle.graphics.drawCircle( 0, 0, 35 );
-    circle.alpha = 0.4;
+    circle.graphics.drawCircle( 0, 0, 28 );
+    circle.alpha = 0.3;
 
-    var plusH = new createjs.Shape();
-    plusH.graphics.beginFill( "#FFF" );
-    plusH.graphics.drawRoundRect( -16, -3, 32, 6, 3, 3, 3, 3 );
+    var plus = new createjs.Shape();
+    plus.graphics.beginFill( "#FFF" );
+    plus.graphics.drawRoundRect( -15, -2, 30, 4, 2, 2, 2, 2 );
+    plus.graphics.drawRoundRect( -2, -15, 4, 30, 2, 2, 2, 2 );
+    plus.scaleX = 0.9;
+    plus.scaleY = 0.9;
 
-    var plusV = new createjs.Shape();
-    plusV.graphics.beginFill( "#FFF" );
-    plusV.graphics.drawRoundRect( -3, -16, 6, 32, 3, 3, 3, 3 );
+    var hitArea = new createjs.Shape();
+    hitArea.graphics.beginFill( "#FFF" );
+    hitArea.graphics.drawRect( -38, -38, 76, 76 );
 
     container.addChild( circle );
-    container.addChild( plusH );
-    container.addChild( plusV );
+    container.addChild( plus );
+
+    container.hitArea = hitArea;
+    container.cursor = "pointer";
+    container.alpha = 0.8;
+
+    container.on( "mouseover", function( event ) {
+        createjs.Tween.get( container )
+            .to( { alpha: 1.0 }, 90, createjs.Ease.cubicInOut() );
+        createjs.Tween.get( circle )
+            .to( { scaleX: 1.1, scaleY: 1.1 }, 350,
+                createjs.Ease.getPowOut( 2.8 ) );
+        createjs.Tween.get( plus )
+            .to( { scaleX: 1.34, scaleY: 1.34 }, 280,
+                createjs.Ease.getBackOut( 4.0 ) );
+    } );
+
+    container.on( "mouseout", function( event ) {
+        createjs.Tween.get( container )
+            .to( { alpha: 0.8 }, 300, createjs.Ease.cubicOut );
+        createjs.Tween.get( circle )
+            .to( { scaleX: 1.0, scaleY: 1.0 }, 300, createjs.Ease.cubicOut );
+        createjs.Tween.get( plus )
+            .to( { scaleX: 0.9, scaleY: 0.9 }, 300, createjs.Ease.cubicOut );
+    } );
 
     return container;
-}
+};
 
 // Build stage
 // -----------------------------------------------------------------------------
 var stage = new createjs.Stage( "stage" );
 createjs.Ticker.setFPS( 60 );
 createjs.Ticker.addEventListener( "tick", stage );
+stage.enableMouseOver();
 
 var displayBackground = new DisplayBackground();
 
@@ -198,22 +293,25 @@ var displayStory = new DisplayStory( "Hello!" );
 displayStory.x = 400;
 displayStory.y = 300;
 
-
 var displayNeighbourPlus1 = new DisplayNeighbourPlus();
-
 displayNeighbourPlus1.x = 400;
 displayNeighbourPlus1.y = 60;
 
 var displayNeighbourPlus2 = new DisplayNeighbourPlus();
-
 displayNeighbourPlus2.x = 400;
 displayNeighbourPlus2.y = 600 - 60;
 
-/* createjs.Tween.get( displayStory, { loop: true } )
-    .to( { scaleX: 4.0, scaleY: 4.0 }, 1000, createjs.Ease.cubicOut() )
-    .to( { scaleX: 1.0, scaleY: 1.0 }, 1000, createjs.Ease.cubicIn() ); */
+var displayNeighbour3 = new DisplayNeighbour( "Do you like pancakes?" );
+displayNeighbour3.x = 640;
+displayNeighbour3.y = 220;
+
+var displayNeighbour4 = new DisplayNeighbour( "I am not a stalker, I swear!!" );
+displayNeighbour4.x = 640;
+displayNeighbour4.y = 380;
 
 stage.addChild( displayBackground );
 stage.addChild( displayStory );
 stage.addChild( displayNeighbourPlus1 );
 stage.addChild( displayNeighbourPlus2 );
+stage.addChild( displayNeighbour3 );
+stage.addChild( displayNeighbour4 );
