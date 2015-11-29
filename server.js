43d4c90b61a96692bfe0a8e6db50bc67d311ce6d
Caio Santana Magalhães <caiosm1005@gmail.com>
2015-11-28 23:30:34 -0300

    Increase socket timeout

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require( "body-parser" );
 // Server configurations
 // -----------------------------------------------------------------------------
 var port = 3014;
+var socketTimeout = 20 * 1000;
 var app = express();
 app.use( express.static( __dirname + "/public" ) );
 app.use( bodyParser.text() );
@@ -13,5 +14,7 @@ var routes = require( "./scripts/routes.js" )( app, model );
 
 // Start server
 // -----------------------------------------------------------------------------
-app.listen( port );
+var server = app.listen( port );
+server.on( "connection", function(socket){ socket.setTimeout(socketTimeout); });
+
 console.log( "hive server listening on port " + port );
